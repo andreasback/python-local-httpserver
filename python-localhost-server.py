fc9bdf0c45610209499007cafdd670d423c2ae71
Andreas Back <back.andreas@gmail.com>
2025-04-12 16:36:51 +0200

    run from command/sh directory and serve files from www/

--- python-localhost-server.py
+++ python-localhost-server.py
@@ -2,6 +2,10 @@ import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 class CustomServer(SimpleHTTPRequestHandler):
+  # https://stackoverflow.com/questions/39801718/how-to-run-a-http-server-which-serves-a-specific-path
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, directory='www', **kwargs)
+
   def do_GET(self):
     path = self.translate_path(self.path)
     #print(f'{self.path} -> {path}')
--- run-local-server.command
+++ run-local-server.command
@@ -1,4 +1,6 @@
 echo $0
 SCRIPT_DIR=$(dirname $0)
 
+cd $SCRIPT_DIR
+
 python3 ${SCRIPT_DIR}/python-localhost-server.py
